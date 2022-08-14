45bf9a7ac612e72b60301f6a476de6e6b7299896
Allyson1602 <aathyrson@yahoo.com.br>
2022-08-14 15:38:24 -0300

    feat: Separação do cabeçalho e do corpo da calculadora na janela

--- main.py
+++ main.py
@@ -16,4 +16,7 @@ janela.config(bg=preto)
 frame_tela = Frame(janela, width=235, height=50)
 frame_tela.grid(row=0, column=0)
 
+frame_corpo = Frame(janela, width=235, height=268)
+frame_corpo.grid(row=1, column=0)
+
 janela.mainloop()
